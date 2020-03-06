392aa6ca4234aff6b0bf6819580397ee2fded1a3
kf03w5t5741l <36108518+kf03w5t5741l@users.noreply.github.com>
2020-03-06 09:59:31 +0100

    Inital commit

---
+++ src/main/java/net/kf03w5t5741l/sensorbase/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package net.kf03w5t5741l.sensorbase.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/kf03w5t5741l/sensorbase/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.kf03w5t5741l.sensorbase.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
