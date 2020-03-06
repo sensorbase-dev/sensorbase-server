12cd088792e0023cfad6c5a9b4782da23c6c602b
kf03w5t5741l <36108518+kf03w5t5741l@users.noreply.github.com>
2020-03-06 12:01:52 +0100

    Add DeviceService class and corresponding DeviceRepository interface

---
+++ src/main/java/net/kf03w5t5741l/sensorbase/server/persistence/DeviceService.java
@@ -0,0 +1,38 @@
+package net.kf03w5t5741l.sensorbase.server.persistence;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import net.kf03w5t5741l.sensorbase.server.domain.device.Device;
+
+@Service
+@Transactional
+public class DeviceService {
+
+    @Autowired
+    private DeviceRepository deviceRepository;
+
+    public Device save(Device device) {
+        return deviceRepository.save(device);
+    }
+
+    public Optional<Device> findById(Long id) {
+        return deviceRepository.findById(id);
+    }
+
+    public Iterable<Device> findAll() {
+        return deviceRepository.findAll();
+    }
+
+    public boolean removeById(Long id) {
+        if (deviceRepository.existsById(id)) {
+            deviceRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
