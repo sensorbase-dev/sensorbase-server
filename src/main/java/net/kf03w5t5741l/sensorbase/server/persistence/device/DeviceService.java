06e7be36de5a81ff919d44a5e6836b4d22f01613
kf03w5t5741l <36108518+kf03w5t5741l@users.noreply.github.com>
2020-03-11 10:19:54 +0100

    Implement deleteAll endpoint for Devices

--- src/main/java/net/kf03w5t5741l/sensorbase/server/persistence/device/DeviceService.java
+++ src/main/java/net/kf03w5t5741l/sensorbase/server/persistence/device/DeviceService.java
@@ -16,27 +16,31 @@ public class DeviceService {
     private DeviceRepository deviceRepository;
 
     public Device save(Device device) {
-        return deviceRepository.save(device);
+        return this.deviceRepository.save(device);
     }
 
     public boolean existsById(Long id) {
-        return deviceRepository.existsById(id);
+        return this.deviceRepository.existsById(id);
     }
 
     public Optional<Device> findById(Long id) {
-        return deviceRepository.findById(id);
+        return this.deviceRepository.findById(id);
     }
 
     public Iterable<Device> findAll() {
-        return deviceRepository.findAll();
+        return this.deviceRepository.findAll();
     }
 
     public boolean deleteById(Long id) {
-        if (deviceRepository.existsById(id)) {
-            deviceRepository.deleteById(id);
+        if (this.deviceRepository.existsById(id)) {
+            this.deviceRepository.deleteById(id);
             return true;
         } else {
             return false;
         }
+    }
+
+    public void deleteAll() {
+        this.deviceRepository.deleteAll();
     }
 }
