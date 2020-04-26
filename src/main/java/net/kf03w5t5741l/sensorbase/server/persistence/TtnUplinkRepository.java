6cc5b0f01c1cf79aadbb79386c47859ee4057da5
kf03w5t5741l <36108518+kf03w5t5741l@users.noreply.github.com>
2020-04-26 16:21:18 +0200

    Add @Component annotations to the uplink repository interfaces

--- src/main/java/net/kf03w5t5741l/sensorbase/server/persistence/TtnUplinkRepository.java
+++ src/main/java/net/kf03w5t5741l/sensorbase/server/persistence/TtnUplinkRepository.java
@@ -2,6 +2,8 @@ package net.kf03w5t5741l.sensorbase.server.persistence;
 
 import net.kf03w5t5741l.sensorbase.server.domain.TtnUplink;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface TtnUplinkRepository extends CrudRepository<TtnUplink, Long> {
 }
--- src/main/java/net/kf03w5t5741l/sensorbase/server/persistence/UbiBotUplinkRepository.java
+++ src/main/java/net/kf03w5t5741l/sensorbase/server/persistence/UbiBotUplinkRepository.java
@@ -3,7 +3,8 @@ package net.kf03w5t5741l.sensorbase.server.persistence;
 
 import net.kf03w5t5741l.sensorbase.server.domain.UbiBotUplink;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface UbiBotUplinkRepository extends CrudRepository<UbiBotUplink, Long> {
-
 }
